f628c847ee9d378da68964661c0e7bf0b6111c74
bk201 <111396011+ang-201@users.noreply.github.com>
2024-08-24 01:32:21 +0000

    Se creo un constructor que inicializa los atributos de la clase DateTypes

--- Exercises/DataTypes.java
+++ Exercises/DataTypes.java
@@ -1,10 +1,32 @@
-package Exercises;
-
 public class DataTypes {
-    public String cadena;
-    public int entero;
-    public float flotante;
-    char caracter;
-    boolean boleano;
+    // Atributos de la clase
+    private String cadena;
+    private int entero;
+    private float flotante;
+    private char caracter;
+    private boolean boleano;
 
+    // Constructor que inicializa los atributos
+    public DataTypes() {
+        this.cadena = "Hola Mundo";
+        this.entero = 10;
+        this.flotante = 5.9f; // 'f' indica que es un float
+        this.caracter = 'A';
+        this.boleano = true;
+    }
+
+    // Métodos para mostrar los valores (opcional)
+    public void mostrarValores() {
+        System.out.println("Cadena: " + cadena);
+        System.out.println("Entero: " + entero);
+        System.out.println("Flotante: " + flotante);
+        System.out.println("Caracter: " + caracter);
+        System.out.println("Boleano: " + boleano);
+    }
+
+    // Método principal para probar la clase
+    public static void main(String[] args) {
+        DataTypes datos = new DataTypes();
+        datos.mostrarValores();
+    }
 }
